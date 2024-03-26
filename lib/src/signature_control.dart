1d2cab8ad86d9bc4067576182742c2c77cb3aae7
natalia <60975802+nzxcvbnm@users.noreply.github.com>
2024-03-26 13:48:02 +0100

    Revert "compress paths"
    This reverts commit 86a5872b7407c6050078bdf82c7895e90e9812fa.

--- lib/src/signature_control.dart
+++ lib/src/signature_control.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 import 'dart:math' as math;
 import 'dart:ui' as ui;
 
@@ -808,20 +807,14 @@ class StoryPainterControl {
     _pathActions.add(PathAction(path: _activePath!, action: Act.add));
   }
 
-  List<int> getPaths() {
-    final String json = jsonEncode(paths.map((e) => e!.toJson()).toList());
-    final List<int> jsonData = utf8.encode(json);
-    final List<int> compressedData = gzip.encode(jsonData);
-    return compressedData;
+  String getPaths() {
+    return jsonEncode(paths.map((e) => e!.toJson()).toList());
   }
 
-  void setPaths(List<int>? compressed) {
-    if (compressed == null) return;
-    final List<int> decompressed = gzip.decode(compressed);
-    final String json = utf8.decode(decompressed);
-    final List<dynamic> decodedList = jsonDecode(json);
-    final List<CubicPath> newPaths =
-        decodedList.map((e) => CubicPath.fromJson(e)).toList();
+  void setPaths(String? string) {
+    if (string == null) return;
+    List<dynamic> list = jsonDecode(string);
+    List<CubicPath> newPaths = list.map((e) => CubicPath.fromJson(e)).toList();
     paths.addAll(newPaths);
   }
 
